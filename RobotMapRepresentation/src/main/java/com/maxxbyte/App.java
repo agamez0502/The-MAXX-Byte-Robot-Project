8e2ac3829258179e1c785ca207fa09867fe21bad
Alondra Gamez <agamez@appdev.yearup.org>
2025-09-02 05:12:09 -0500

    feat: added MapNode class with fields and constructor. This is the smallest piece of data (coordinates + type).

---
+++ RobotMapRepresentation/src/main/java/com/maxxbyte/App.java
@@ -0,0 +1,8 @@
+package com.maxxbyte;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ RobotMapRepresentation/src/main/java/com/maxxbyte/MapNode.java
@@ -0,0 +1,44 @@
+package com.maxxbyte;
+
+public class MapNode {
+
+    //properties=================================================================================
+    // represents one point on the map
+    private int x;
+    private int y;
+    private String type; // "building", "path", "obstacle"
+
+    //constructor================================================================================
+    public MapNode(int x, int y, String type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    //methods====================================================================================
+
+    //getters and setters========================================================================
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
